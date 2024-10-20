97a83e0084801696045f2f5f86bfacd1186aa2b3
Jackk552 <jacksonalee@gmail.com>
2024-10-20 23:17:58 +0800

    functionality test

--- index.js
+++ index.js
@@ -1,7 +1,27 @@
-document.getElementById("flash").addEventListener("click", myFunc);
+const flashcardsForm = document.querySelector('.forms');
+const flashcardsContainer =  document.querySelector('.flashcards');
+const flashcardFront = flashcardForm['front'];
+const flashcardBack = flashcardForm['back'];
 
-function myFunc()
-{
-    document.getElementById("flash").innerHTML = "New text!";
-}
-console.log("waterhell")
+const flashcards = [{
+    front: "What the fu?",
+    back: "yes"
+}];
+
+const addFlashcards = (front, back) => {
+
+};
+
+const createFlashcardElement = ({front, back}) =>{
+    const flashcardDiv = document.createElement('div');
+    const flashcardFront = document.createElement('p');
+    const flashcardBack = document.createElement('p');
+
+    flashcardFront.innerText = front;
+    flashcardBack.innerText = back;
+
+    flashcardDiv.append(flashcardFront, flashcardBack);
+    flashcardContainer.appendChild(flashcardDiv); 
+};
+
+flashcards.forEach(createFlashcardElement)
