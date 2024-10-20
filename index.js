728347196900e16f3b8e38507bfe4103dce9ea36
Jackk552 <jacksonalee@gmail.com>
2024-10-20 21:37:00 +0800

    add UI

---
+++ index.js
@@ -0,0 +1,7 @@
+document.getElementById("flash").addEventListener("click", myFunc);
+
+function myFunc()
+{
+    document.getElementById("flash").innerHTML = "New text!";
+}
+console.log("waterhell")
